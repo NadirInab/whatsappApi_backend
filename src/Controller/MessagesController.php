4e87c5cbfeec98fdbf167e3ccad41d5adf615725
nadir inab <nadir.inab.dev@gmail.com>
2023-10-31 16:29:58 +0100

    lines were added

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Websocket\MessageHandler;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class MessagesController extends AbstractController
 {
+
+    protected $messageHandler;
+
+    public function __construct(MessageHandler $messageHandler)
+    {
+        $this->messageHandler = $messageHandler;
+    }
+
     #[Route('/api', methods: ['GET'])]
     public function index(): JsonResponse
     {
@@ -20,40 +32,10 @@ class MessagesController extends AbstractController
         ]);
     }
 
-    // Send a message to WhatsApp
     #[Route('/api/sendMessage', methods: ['POST'])]
     public function sendMessage(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-
-        // =============================>
-
-        // $payload = [
-        //     "messaging_product" => "whatsapp",
-        //     "recipient_type" => "individual",
-        //     "to" => "212636740837",
-        //     "type" => "template",
-        //     "template" => [
-        //         "name" => "imane",
-        //         "language" => [
-        //             "code" => "fr"
-        //         ],
-        //         "components" => [
-        //             [
-        //                 "type" => "header",
-        //                 "parameters" => [
-        //                     [
-        //                         "type" => "image",
-        //                         "image" => [
-        //                             "link" => "https://play-lh.googleusercontent.com/lC50PAc7_DmaGZrE01f7jVPMTjoYqEzuC59D3zj77ZOk5HlKGybX1dOMyWrGvXMHf1Yr"
-        //                         ]
-        //                     ]
-        //                 ]
-        //             ]
-        //         ]
-        //     ]
-        // ];
-
         $payload = [
             'messaging_product' => 'whatsapp',
             'to' => $data['to'],
@@ -62,10 +44,8 @@ class MessagesController extends AbstractController
                 'body' => $data['text']['body']
             ]
         ];
-
-
         $apiEndpoint =  "https://graph.facebook.com/v17.0/100206783144220/messages";
-        $accessToken = "EAACP9wBzdvEBOyc8Rep52y19BZABdhYtckTMIOZBbwLQErjTvM227xWoiEZBmqazQM5pDzx1WOabhWxpyQxKRQcC7z9ldBHt8vZCm5R00j4X91Ux8vIuxxuO8GLTsRSIB0nvPZA9CfmKuVm389lsRlYdmiFHLHYcGnG70UswgJQ4QTeBxIulp2UliuHw7q4jWicdLALZBc7CefSsWlyj1MyRv3HygZD";
+        $accessToken = "EAACP9wBzdvEBOZB1iwDL7HmjOAkD1gb6ZAxjemglalOe1q0Rh7UHXtNXnbgzaAI1E0H2no1OuFWBUVqzbvG6B6zj9gdwO67AWC1g7HM9f07EwFTwuQNyRhnWXzZA5O4puOqtVlowgRP53MD4RLunzbKzUuyuMwXW0udhn2k2jfUXuv364cBTjNfVD8GugDW7FnyjBOb3Nfz5kJVoPS6AMMtVZBsZD";
 
         try {
             $client = HttpClient::create();
@@ -87,14 +67,6 @@ class MessagesController extends AbstractController
         }
     }
 
-    // Receive messages from WhatsApp webhook
-    // #[Route('/api/receiveMessage', methods: ['POST'])]
-    // public function receiveMessage(Request $request): JsonResponse
-    // {
-    //     $data = json_decode($request->getContent(), true);
-    //     return new JsonResponse($data, 200);
-    // }
-
     // #[Route('/api/webhooks', methods: ['GET'])]
     // public function handleWebhook(Request $request): JsonResponse
     // {
@@ -113,68 +85,121 @@ class MessagesController extends AbstractController
     //         return new JsonResponse((int) $challenge);
     //     }
 
-    //     return new JsonResponse('Invalid request method');
+    //     return new JsonResponse('Invalid request method'); StreamedResponse
     // }
 
 
     #[Route('/api/webhooks', methods: ['GET', 'POST'])]
-    public function getMessageResponse(Request $request): JsonResponse
+    public function getMessageResponse(Request $request, HubInterface $hub): JsonResponse
     {
 
         $jsonContent = $request->getContent();
         $data = json_decode($jsonContent, true);
-        error_log("Received WhatsApp Data Here !!!! :====>  " . print_r($data, true));    
+        error_log("Received WhatsApp Data Here !!!! :====>  " . print_r($data, true));
 
-        error_log("===========================> !!") ;
-        $object = $data['object']; 
-        $entry = $data['entry'][0]; 
+        // if (!isset($data['messages'])) {
+        //     error_log("Received WhatsApp Data Here !!!! :====>  " . print_r($data, true));    
 
-        $id = $entry['id']; 
+        //     error_log("===========================> !!");
 
-        error_log("here we are ". $id) ;
+        //     $object = $data['object'];
+        //     $entry = $data['entry'][0];
+        //     $id = $entry['id'];
 
-        $changes = $entry['changes'][0]; 
-        $value = $changes['value']; 
-        $messagingProduct = $value['messaging_product']; 
+        //     if (isset($entry['changes'][0])) {
+        //         $changes = $entry['changes'][0];
+        //         $value = $changes['value'];
 
-        $metadata = $value['metadata']; 
-        $displayPhoneNumber = $metadata['display_phone_number'];
-        $phoneNumberId = $metadata['phone_number_id'];
+        //         if (isset($value['messaging_product'])) {
+        //             $messagingProduct = $value['messaging_product'];
 
-        $contacts = $value['contacts'][0]; 
+        //             if (isset($value['metadata'])) {
+        //                 $metadata = $value['metadata'];
 
-        $profile = $contacts['profile']; 
-        $name = $profile['name']; 
-        error_log("here we are ". $name) ;
+        //                 if (isset($metadata['display_phone_number'])) {
+        //                     $displayPhoneNumber = $metadata['display_phone_number'];
+        //                 }
+        //                 if (isset($metadata['phone_number_id'])) {
+        //                     $phoneNumberId = $metadata['phone_number_id'];
+        //                 }
+        //             }
 
-        $waId = $contacts['wa_id']; 
+        //             if (isset($value['contacts'][0])) {
+        //                 $contacts = $value['contacts'][0];
 
-        $messages = $value['messages'][0];
+        //                 if (isset($contacts['profile'])) {
+        //                     $profile = $contacts['profile'];
 
-        $from = $messages['from'];
-        $id = $messages['id']; 
-        $timestamp = date('m/d/Y H:i:s',$messages['timestamp']) ; 
+        //                     if (isset($profile['name'])) {
+        //                         $name = "Nadir";
+        //                     }
+        //                 }
 
-        $text = $messages['text']; 
-        $body = $text['body'];
-        $type = $messages['type'];
+        //                 if (isset($contacts['wa_id'])) {
+        //                     $waId = $contacts['wa_id'];
+        //                 }
+        //             }
+
+        //             if (isset($value['messages'][0])) {
+        //                 $messages = $value['messages'][0];
+
+        //                 if (isset($messages['from'])) {
+        //                     $from = $messages['from'];
+        //                 }
+        //                 if (isset($messages['id'])) {
+        //                     $id = $messages['id'];
+        //                 }
+        //                 if (isset($messages['timestamp'])) {
+        //                     $timestamp = date('m/d/Y H:i:s', $messages['timestamp']);
+        //                 }
+        //                 if (isset($messages['text']) && isset($messages['text']['body'])) {
+        //                     $text = $messages['text'];
+        //                     $body = $text['body'];
+        //                 }
+        //                 if (isset($messages['type'])) {
+        //                     $type = $messages['type'];
+        //                 }
+        //             }
+        //         }
+        //     }}
+
+        // $responseData = [
+        //     'object' => $object,
+        //     'id' => $id,
+        //     'messagingProduct' => $messagingProduct,
+        //     'displayPhoneNumber' => $displayPhoneNumber,
+        //     'phoneNumberId' => $phoneNumberId,
+        //     'name' => $name,
+        //     'waId' => $waId,
+        //     'from' => $from,
+        //     'messageId' => $id,
+        //     'timestamp' => $timestamp,
+        //     'messageText' => $body,
+        //     'messageType' => $type,
+        // ];
 
         $responseData = [
-            'object' => $object,
-            'id' => $id,
-            'messagingProduct' => $messagingProduct,
-            'displayPhoneNumber' => $displayPhoneNumber,
-            'phoneNumberId' => $phoneNumberId,
-            'name' => $name,
-            'waId' => $waId,
-            'from' => $from,
-            'messageId' => $id,
-            'timestamp' => $timestamp,
-            'messageText' => $body,
-            'messageType' => $type,
+            'object' => "nadir",
+            'id' => 1234,
+            'messagingProduct' => "whatsapp",
+            'displayPhoneNumber' => "212636740837",
+            'phoneNumberId' => 1234,
+            'name' => "nadir",
+            'waId' => 1233,
+            'from' => "test",
+            'messageId' => 1234,
+            'timestamp' => 123456787,
+            'messageText' => "message seconde!!",
+            'messageType' => "type",
         ];
 
-        return new JsonResponse(['data' =>$responseData ], 200);
+        $update = new Update(
+            'http://127.0.0.1:5173/',
+            json_encode(['status' => 'Here we are !!!'])
+        );
 
+        $hub->publish($update);
+
+        return new JsonResponse('published!');
     }
 }
---
+++ src/Controller/SSEController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SSEController extends AbstractController
+{
+    /**
+     * @Route("/sse/whatsapp", name="sse_whatsapp")
+     */
+
+    public function whatsapp(Request $request)
+    {
+        $response = new Response('', Response::HTTP_OK, ['Content-Type' => 'text/event-stream']);
+        $response->headers->set('Cache-Control', 'no-cache');
+
+        $response->sendHeaders();
+
+        while ($request) {
+            // Listen for new messages from WhatsApp and send them to the client
+            $messages = []; // Get the new messages from WhatsApp
+
+            foreach ($messages as $message) {
+                $response->setContent('event: message' . PHP_EOL . 'data: ' . json_encode($message) . PHP_EOL . PHP_EOL);
+                $response->send();
+            }
+
+            flush();
+        }
+
+        return $response;
+    }
+}
